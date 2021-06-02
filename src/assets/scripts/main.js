72a5b4b4ba92aaf4b73ca049e9e9b54dd8fc2592
williamrjribeiro <will@williamrjribeiro.com>
2021-06-02 01:11:28 +0100

    Show sticky avatar in mobile & scroll to top

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,22 +2,33 @@ import 'focus-visible'
 
 document.documentElement.classList.remove('no-js')
 
-// Scroll State
-const onScroll = () => {
-    const scrollClassName = 'js-scrolled'
-    const scrollTreshold = 200
-    const isOverTreshold = window.scrollY > scrollTreshold
+const onScroll =
+    ({ scrollClassName, scrollTreshold }) =>
+    () => {
+        const isOverTreshold = window.scrollY > scrollTreshold
 
-    if (isOverTreshold) {
-        document.documentElement.classList.add(scrollClassName)
-    } else {
-        document.documentElement.classList.remove(scrollClassName)
+        if (isOverTreshold) {
+            document.documentElement.classList.add(scrollClassName)
+        } else {
+            document.documentElement.classList.remove(scrollClassName)
+        }
     }
+
+const onScrollOptions = {
+    scrollClassName: 'js-scrolled',
+    scrollTreshold: 200
 }
-window.addEventListener('scroll', onScroll, { passive: true })
+
+window.addEventListener('scroll', onScroll(onScrollOptions), { passive: true })
+
+// When the user clicks on the avatar, scroll to the top of the document
+document.querySelector('.vcard__avatar').addEventListener('click', () => {
+    console.log('clicked on avatar!')
+    document.body.scrollTop = 0 // For Safari
+    document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+})
 
 // Print Button
-const printButton = document.querySelector('.js-print')
-printButton.addEventListener('click', () => {
+document.querySelector('.js-print').addEventListener('click', () => {
     window.print()
 })
